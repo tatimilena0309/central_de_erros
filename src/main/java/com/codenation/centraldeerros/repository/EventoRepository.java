eb0328ffa6fabf47e5fda51aef71a6186cd6c4cc
tatimilena0309 <tatiiane.martiins@gmail.com>
2020-06-20 11:22:44 -0300

    Alteracoes Repository e Enum level para facilitar o sort e ordenacao

--- src/main/java/com/codenation/centraldeerros/repository/EventoRepository.java
+++ src/main/java/com/codenation/centraldeerros/repository/EventoRepository.java
@@ -2,18 +2,16 @@ package com.codenation.centraldeerros.repository;
 
 import com.codenation.centraldeerros.enums.Level;
 import com.codenation.centraldeerros.model.Evento;
-import com.google.gson.JsonElement;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-
-import java.sql.Timestamp;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
 
-public interface EventoRepository extends CrudRepository<Evento, Long> {
+//public interface EventoRepository extends CrudRepository<Evento, Long> {
+public interface EventoRepository extends PagingAndSortingRepository<Evento, Long> {
+
     Page<Evento> findAll(Pageable pageable);
 
     Evento save(Evento evento);
@@ -29,4 +27,20 @@ public interface EventoRepository extends CrudRepository<Evento, Long> {
     @Query(" select e.log from Evento e " +
             " where e.id = :id ")
     Optional<String> findByIdLog(Long id);
+
+    //FUNCTION get_text (p_param IN VARCHAR2) RETURN VARCHAR2
+   /* @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public String callGetTextFunction(String param) {
+        SimpleJdbcCall jdbcCall = new SimpleJdbcCall(jdbcTemplate)
+                .withCatalogName("PKG_PACKAGE") //package name
+                .withFunctionName("GET_TEXT"); //function name
+        SqlParameterSource paramMap = new MapSqlParameterSource()
+                .addValue("p_param", param));
+        //First parameter is function output parameter type.
+        return jdbcCall.executeFunction(String.class, paramMap));
+    }*/
+
 }
+
